1a3e5828bf2d9e9487f67b57e9ec372949922df3
junaidali-ruk <junaidruk375@gmail.com>
2024-12-01 19:13:32 +0500

    commit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-console.log(typeof Infinity);
-console.log(typeof NaN);
-console.log(typeof undefined);
-console.log(typeof null);
+console.log(typeof Infinity);  
+console.log(typeof NaN);  
+console.log(typeof undefined);  
+console.log(typeof null);  
 
-//create a table in browser console
+// Create a table in the browser console
+let name;
 console.table({
   name: "John",
   age: 30,
   city: "New York",
 });
+
+ 
+name = "Jane";
+console.log(name);  
+
+// Arithmetic operations
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log("Sum:", sum);
+let difference = y - x;
+console.log("Difference:", difference);
