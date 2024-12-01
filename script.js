18b1044c2671522b2bd87bd05414e99b91aa9401
junaidali-ruk <junaidruk375@gmail.com>
2024-12-01 17:24:32 +0500

    commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log(null + undefined);
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(typeof undefined);
+console.log(typeof null);
 
+//create a table in browser console
+console.table({
+  name: "John",
+  age: 30,
+  city: "New York",
+});
