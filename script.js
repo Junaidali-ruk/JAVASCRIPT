2b4b08511fce9ee0a143d0f690e90a7fa97206ce
junaidali-ruk <junaidruk375@gmail.com>
2024-12-03 18:00:32 +0500

    commit

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const bigNumber = 99347647438578549854589n
+let bigNumber = 99347647438578549854589n;
 console.log(bigNumber);
 // BigInt is a new primitive data type that allows you to represent whole numbers larger than 2^53 - 1.
 
-const bigNumber2 = BigInt(99347647438578549854589)
+let bigNumber2 = BigInt(99347647438578549854589);
 console.log(bigNumber2);
 // You can also create a BigInt from a string.
 
-const name = "John"
-const name2 = name
-const name3 = name2
-
+let name = "John";
+let name2 = name;
+let name3 = name2;
+name="junaid"
 console.log(name3);
+
+let obj = {
+  name: "John",
+  age: 30,
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+let obj2 = obj;
+
+console.log(obj2.name="junaid");
