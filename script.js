e76c958156d8c308a3e4f1e2b6736c268dec6b23
junaidali-ruk <junaidruk375@gmail.com>
2024-12-04 17:11:52 +0500

    commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Working with BigInt
+
 let largeNumber1 = 1234567890123456789012345678901234567890n;
 console.log(largeNumber1);
 
 let largeNumber2 = BigInt("1234567890123456789012345678901234567890");
 console.log(largeNumber2);
 
-// Playing with variable references
+
 let color = "Blue";
 let color2 = color;
 let color3 = color2;
 color = "Green";
-console.log(color3); // Outputs "Blue"
+console.log(color3);
 
-// Working with object references
+
 let person = {
   name: "Alice",
   age: 25,
@@ -24,7 +24,7 @@ let person = {
 
 let person2 = person;
 
-// Modifying person2 also affects person
+
 person2.hobbies.indoor = "Painting";
-console.log(person.hobbies.indoor); // Outputs "Painting"
-console.log(person2.name = "Bob");  // Modifies and prints "Bob"
+console.log(person.hobbies.indoor); 
+console.log(person2.name = "junaid");
