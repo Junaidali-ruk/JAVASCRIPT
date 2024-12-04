074e37abb8d3bc85f58ad920845613b426616f79
junaidali-ruk <junaidruk375@gmail.com>
2024-12-04 17:39:38 +0500

    commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let largeNumber1 = 1234567890123456789012345678901234567890n;
 console.log(largeNumber1);
 
@@ -28,3 +27,7 @@ let person2 = person;
 person2.hobbies.indoor = "Painting";
 console.log(person.hobbies.indoor); 
 console.log(person2.name = "junaid");
+
+const arr=[2,1,2,3,4,5,6,7]
+const random= Math.floor(Math.random()*arr.length)
+console.log(arr[random])
